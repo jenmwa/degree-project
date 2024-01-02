99f90999a40c815ab3abd963fa51d1572f2727d3
jenmwa <jenmwa@gmail.com>
2024-01-02 14:45:55 +0100

    Add - database READ connection

--- app/butiken/page.tsx
+++ app/butiken/page.tsx
@@ -1,4 +1,36 @@
+"use client";
+import { createClient } from "@supabase/supabase-js";
+import { useEffect } from "react";
+
 const Butiken = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+      const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
+
+      if (!supabaseUrl || !supabaseKey) {
+        console.error("Supabase URL or key is missing.");
+      } else {
+        const supabase = createClient(supabaseUrl, supabaseKey);
+        try {
+          const { data: products, error } = await supabase
+            .from("Product")
+            .select("*");
+
+          if (error) {
+            console.error("Error fetching data:", error);
+          } else {
+            console.log("Products:", products);
+          }
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       {" "}
