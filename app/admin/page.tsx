e0f9dda9991ab1a54b9f94728f227217a977568a
jenmwa <jenmwa@gmail.com>
2024-01-22 17:14:11 +0100

    .

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,24 +4,24 @@ import { useState } from "react";
 import { supabase } from "../../lib/supabase";
 
 export default function Admin() {
-  const [loading, setLoading] = useState(false);
-  const [email, setEmail] = useState("");
+  // const [loading, setLoading] = useState(false);
+  // const [email, setEmail] = useState("");
 
-  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  // const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
 
-    setLoading(true);
-    const { error } = await supabase.auth.signInWithOtp({
-      email,
-    });
+  //   setLoading(true);
+  //   const { error } = await supabase.auth.signInWithOtp({
+  //     email,
+  //   });
 
-    if (error) {
-      alert(error.status || error.message);
-    } else {
-      alert("Check your email for the login link!");
-    }
-    setLoading(false);
-  };
+  //   if (error) {
+  //     alert(error.status || error.message);
+  //   } else {
+  //     alert("Check your email for the login link!");
+  //   }
+  //   setLoading(false);
+  // };
 
   return (
     <>
@@ -29,7 +29,8 @@ export default function Admin() {
       <br />
       <br />
       <br />
-      <form onSubmit={handleLogin}>
+      <p>admin</p>
+      {/* <form onSubmit={handleLogin}>
         <label htmlFor="email">Email</label>
         <input
           value={email}
@@ -40,7 +41,7 @@ export default function Admin() {
         <button disabled={loading}>
           {loading ? "Sending magic link..." : "Send magic link"}
         </button>
-      </form>
+      </form> */}
     </>
   );
 }
