db70ed17c52c382a939162e34fd3fc8cb177cc22
jenmwa <jenmwa@gmail.com>
2024-01-22 16:25:43 +0100

    fix test

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -11,7 +11,7 @@ import { IBooking } from "@/app/_models/IBooking";
 import EditProduct from "@/app/_components/EditProduct";
 import { initialProduct } from "@/app/initialProduct";
 
-export const Dashboard = () => {
+const Dashboard = () => {
   console.log("hello admin");
   const { products, isLoading, isError } = useProductContext();
   const [selectedProduct, setSelectedProduct] =
