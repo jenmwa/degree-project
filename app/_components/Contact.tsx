ee07e1ae70f8f3812abb5dfacd7e666d739e56d3
jenmwa <jenmwa@gmail.com>
2024-01-22 13:13:34 +0100

    fix vercel error deploy

--- app/_components/Contact.tsx
+++ app/_components/Contact.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(" ");
 }
-const Contact = () => {
+export function Contact() {
   const [agreed, setAgreed] = useState(false);
 
   return (
@@ -198,4 +198,4 @@ const Contact = () => {
       </div>
     </>
   );
-};
+}
