fd93771fabe795fd13119ceedab71fa23f96eca1
jenmwa <jenmwa@gmail.com>
2024-01-29 15:05:42 +0100

    ADD imageCarousel swiper

--- app/_components/ImageCarousel.tsx
+++ app/_components/ImageCarousel.tsx
@@ -1,160 +1,96 @@
 "use client";
-import Image from "next/image";
+import React, { useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "../styles/_swiper.css";
+
 import img from "../../public/img/hero.png";
+import Image from "next/image";
+
+const imgArray = [img, img, img];
 
 export default function ImageCarousel() {
+  useEffect(() => {
+    const handlePaginationKeyPress = (event: KeyboardEvent) => {
+      if (event.key === "Enter" || event.key === " ") {
+        const button = event.target as HTMLButtonElement;
+        button.click();
+      }
+    };
+
+    const paginationButtons = document.querySelectorAll<HTMLButtonElement>(
+      ".swiper-pagination-bullet"
+    );
+
+    paginationButtons.forEach((button) => {
+      button.tabIndex = 0;
+      button.addEventListener("keydown", handlePaginationKeyPress);
+      button.setAttribute("role", "button");
+      button.setAttribute("aria-label", "Go to slide");
+    });
+
+    const handleNavigationKeyPress = (event: KeyboardEvent) => {
+      if (event.key === "Enter" || event.key === " ") {
+        const button = event.target as HTMLButtonElement;
+        button.click();
+      }
+    };
+
+    const navigationButtons = document.querySelectorAll<HTMLButtonElement>(
+      ".swiper-button-prev, .swiper-button-next"
+    );
+
+    navigationButtons.forEach((button) => {
+      button.tabIndex = 0;
+      button.addEventListener("keydown", handleNavigationKeyPress);
+      button.setAttribute("role", "button");
+      button.setAttribute(
+        "aria-label",
+        button.classList.contains("swiper-button-prev")
+          ? "Föregående Slide"
+          : "Nästa Slide"
+      );
+    });
+
+    return () => {
+      paginationButtons.forEach((button) => {
+        button.removeEventListener("keydown", handlePaginationKeyPress);
+      });
+
+      navigationButtons.forEach((button) => {
+        button.removeEventListener("keydown", handleNavigationKeyPress);
+      });
+    };
+  }, []);
+
   return (
     <>
-      <Image src={img} className="" alt="..." width={400} height={100} />
-
-      <div
-        id="indicators-carousel"
-        className="relative w-full"
-        data-carousel="static"
-        style={{ border: "4px solid black" }}
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
       >
-        <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-          <div
-            className="hidden duration-700 ease-in-out"
-            data-carousel-item="active"
-          >
+        {imgArray.map((img, index) => (
+          <SwiperSlide key={index}>
             <Image
               src={img}
-              // className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-              width={400}
-              height={100}
+              alt={`Slide ${index + 1}`}
+              width={300}
+              height={200}
             />
-          </div>
-
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image
-              src={img}
-              // className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-              width={400}
-              height={100}
-            />
-          </div>
-
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image
-              src={img}
-              // className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-              width={400}
-              height={100}
-            />
-          </div>
-
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image
-              src={img}
-              // className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-              width={400}
-              height={100}
-            />
-          </div>
-
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image
-              src={img}
-              // className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-              width={400}
-              height={100}
-            />
-          </div>
-        </div>
-
-        <div className="absolute z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2">
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full bg-black"
-            aria-current="true"
-            aria-label="Slide 1"
-            data-carousel-slide-to="0"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full bg-black"
-            aria-current="false"
-            aria-label="Slide 2"
-            data-carousel-slide-to="1"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full bg-black"
-            aria-current="false"
-            aria-label="Slide 3"
-            data-carousel-slide-to="2"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full bg-black"
-            aria-current="false"
-            aria-label="Slide 4"
-            data-carousel-slide-to="3"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full bg-black"
-            aria-current="false"
-            aria-label="Slide 5"
-            data-carousel-slide-to="4"
-          ></button>
-        </div>
-
-        <button
-          type="button"
-          className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-prev
-        >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-black dark:text-gray-800 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M5 1 1 5l4 4"
-              />
-            </svg>
-            <span className="sr-only">Previous</span>
-          </span>
-        </button>
-        <button
-          type="button"
-          className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-next
-        >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-black dark:text-gray-800 rtl:rotate-180"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 9 4-4-4-4"
-              />
-            </svg>
-            <span className="sr-only">Next</span>
-          </span>
-        </button>
-      </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   );
 }
