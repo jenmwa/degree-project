52eecde46ffe9078108c28c31679e85f4ed7d619
jenmwa <jenmwa@gmail.com>
2024-01-31 23:13:26 +0100

    FIX heroImg error

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,21 +1,11 @@
 "use client";
-import { useState } from "react";
+
 import Image from "next/image";
 import logo from "../../public/bjorbyblomster_logo.svg";
-import { useMediaQuery } from "react-responsive";
-
-import MenuOpen from "./MenuOpen";
-import { Nav } from "./Nav";
 import Link from "next/link";
 
 export function Hero() {
-  // const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  const isLGScreen = useMediaQuery({ minWidth: 1024 });
-
-  // const menuOpenClose = () => {
-  //   console.log("click menu");
-  //   setMobileMenuOpen(!mobileMenuOpen);
-  // };
+  const imageSizeSM = 200;
 
   return (
     <div className="bg-cover bg-no-repeat sm:bg-center lg:bg-top relative bg-hero ">
@@ -23,35 +13,23 @@ export function Hero() {
         <div className="bg-blob" />
       </div>
 
-      {/* <header className="absolute inset-x-0 top-0 z-50">
-        <Nav menuOpenClose={menuOpenClose} navigation={navigation}></Nav>
-
-        <MenuOpen
-          navigation={navigation}
-          menuOpenClose={menuOpenClose}
-          mobileMenuOpen={mobileMenuOpen}
-        ></MenuOpen>
-      </header> */}
-
       <div className="relative isolate px-6 pt-14 lg:px-8 text-dark">
         <div className="mx-auto max-w-2xl py-32 sm:py-36 lg:py-56 flex flex-col lg:max-w-4xl lg:flex-row-reverse justify-between lg:gap-10">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center m:w-1/4 lg:w-1/2 relative ">
-            {isLGScreen ? (
-              <Image
-                src={logo}
-                alt="logo"
-                fill={true}
-                className="lg:w-200 lg:h-200 xl:w-400 xl:h-400 object-contain"
-              ></Image>
-            ) : (
-              <Image
-                src={logo}
-                alt="logo"
-                width={200}
-                height={200}
-                className="lg:w-200 lg:h-200 xl:w-400 xl:h-400 object-contain"
-              ></Image>
-            )}
+          <div className="hidden lg:flex sm:mb-8 sm:flex sm:justify-center m:w-1/4 lg:w-1/2 relative ">
+            <Image
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              src={logo}
+              alt="logo"
+            />
+
+            <Image
+              src={logo}
+              alt="logo"
+              width={imageSizeSM}
+              height={imageSizeSM}
+              className="lg:hidden xl:hidden lg:w-200 lg:h-200 xl:w-400 xl:h-400 object-contain"
+            />
           </div>
 
           <div className="text-center sm:text-left lg:w-1/2">
