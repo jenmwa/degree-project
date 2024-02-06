62c91c2f5968ff020e6b84328c77e8edb65b7569
jenmwa <jenmwa@gmail.com>
2024-02-06 02:31:53 +0100

    FIX tests

--- __tests__/login.test.tsx
+++ __tests__/login.test.tsx
@@ -1,17 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import Login from "/Users/jennywaller/Documents/degree-project/app/login/page";
-
 import "@testing-library/jest-dom";
-import { AppRouterContextProviderMock } from "/Users/jennywaller/Documents/degree-project/__mocks__/app-router-context-provider-mock";
 
 describe("Login Form", () => {
   it("should have required fields and submit button", () => {
-    const push = jest.fn();
-    render(
-      // <AppRouterContextProviderMock router={{ push }}>
-      <Login />
-      // </AppRouterContextProviderMock>
-    );
+    render(<Login />);
 
     const emailField = screen.getByLabelText("Epost");
     const passwordField = screen.getByLabelText("Lösenord");
