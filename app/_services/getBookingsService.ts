c8398ae00e368da0abe06082db0b21fb3f68ea75
jenmwa <jenmwa@gmail.com>
2024-02-11 23:11:04 +0100

    FIX bookingdata is fetched

--- app/_services/getBookingsService.ts
+++ app/_services/getBookingsService.ts
@@ -1,13 +1,13 @@
 
+
 export async function getBookingsService() {
-  {
-    try {
-      const response = await fetch("/api/getBookingsWithUsers");
-      const data = await response.json();
-      console.log(data.data)
-      return data.data
-    } catch (error) {
-      console.error("Error fetching bookings:", error);
-    }
+  try {
+    const response = await fetch("/api/handlers?entity=Booking");
+    console.log('response is', response)
+    const data = await response.json();
+    console.log('data is: ', data, data.data)
+    return data.data
+  } catch (error) {
+    console.error("Error fetching products:", error);
   }
 }
--- pages/api/getBookingsWithUser.ts
+++ pages/api/getBookingsWithUser.ts
@@ -1,58 +1,77 @@
-
-
-import { supabaseAuthClient } from "../../lib/supabaseAuthClient";
+import { supabaseAuthClient } from "lib/supabaseAuthClient";
 
 export default async function getBookingsWithUsers(req: any, res: any) {
-  // const { entity } = req.query;
-
   if (req.method === 'GET') {
     try {
-      let { data, error } = await supabaseAuthClient
+      let { data: Booking, error } = await supabaseAuthClient
         .from('Booking')
         .select('*');
 
       if (error) {
         throw error;
       }
-
-      if (data) {
-        const bookingsWithCustomerEmailAndTitle = [];
-        for (const booking of data) {
-          const { data: customerData, error: customerError } = await supabaseAuthClient
-            .from('User')
-            .select('userEmail')
-            .eq('userId', booking.customer);
-
-          if (customerError) {
-            throw customerError;
-          }
-
-          const { data: productData, error: productError } = await supabaseAuthClient
-            .from('Product')
-            .select('productTitle')
-            .eq('productId', booking.product);
-
-          if (productError) {
-            throw productError;
-          }
-
-          const bookingWithCustomerEmailAndTitle = {
-            ...booking,
-            customerEmail: customerData[0].userEmail,
-            productTitle: productData[0].productTitle
-          };
-          bookingsWithCustomerEmailAndTitle.push(bookingWithCustomerEmailAndTitle);
-        }
-
-        res.status(200).json({ data: bookingsWithCustomerEmailAndTitle });
-      } else {
-        res.status(200).json({ data });
-      }
+      res.status(200).json({ data: Booking });
     } catch (error) {
-      res.status(500).json({ error: 'Error fetching data' });
+      console.error('error', error)
+      res.status(500).json({ error: 'An error occurred while fetching bookings.' });
     }
-  } else {
-    res.status(405).json({ error: 'Method Not Allowed' });
   }
-};
+
+}
+
+// import { supabaseAuthClient } from "../../lib/supabaseAuthClient";
+
+// export default async function getBookingsWithUsers(req: any, res: any) {
+//   // const { entity } = req.query;
+
+//   if (req.method === 'GET') {
+//     try {
+//       let { data, error } = await supabaseAuthClient
+//         .from('Booking')
+//         .select('*');
+
+//       if (error) {
+//         throw error;
+//       }
+
+//       if (data) {
+//         const bookingsWithCustomerEmailAndTitle = [];
+//         for (const booking of data) {
+//           const { data: customerData, error: customerError } = await supabaseAuthClient
+//             .from('User')
+//             .select('userEmail')
+//             .eq('userId', booking.customer);
+
+//           if (customerError) {
+//             throw customerError;
+//           }
+
+//           const { data: productData, error: productError } = await supabaseAuthClient
+//             .from('Product')
+//             .select('productTitle')
+//             .eq('productId', booking.product);
+
+//           if (productError) {
+//             throw productError;
+//           }
+
+//           const bookingWithCustomerEmailAndTitle = {
+//             ...booking,
+//             customerEmail: customerData[0].userEmail,
+//             productTitle: productData[0].productTitle
+//           };
+//           bookingsWithCustomerEmailAndTitle.push(bookingWithCustomerEmailAndTitle);
+//         }
+
+//         res.status(200).json({ data: bookingsWithCustomerEmailAndTitle });
+//       } else {
+//         res.status(200).json({ data });
+//       }
+//     } catch (error) {
+//       res.status(500).json({ error: 'Error fetching data' });
+//     }
+//   } else {
+//     res.status(405).json({ error: 'Method Not Allowed' });
+//   }
+// };
 
